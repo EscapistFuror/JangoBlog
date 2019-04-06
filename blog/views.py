51c0ac7fb0c2a409fde3d48ada81b0fcff390c8a
Hashu <Hashu@me.com>
2019-04-06 18:49:31 +0530

    players

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
+from .models import Team,Player
 # Create your views here.
 
 def Team_List(request):
-    return render(request, 'blog/Team_List.html', {})
+    Teams=Team.objects.all()
+    return render(request, 'blog/Team_List.html', {'Teams' : Teams})
+
+def Player_List(request):
+    Players=Player.objects.all()
+    return render(request, 'blog/Player_List.html',{'Players' : Players})
+
